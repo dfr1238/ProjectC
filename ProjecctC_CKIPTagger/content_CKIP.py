83131414a19845610cbb36acacd8d82aee3fdaf1
Douggy Sans <dfr1238@gmail.com>
2020-05-15 15:44:20 +0800

    新增訓練資料路徑
    新增統一路徑

--- ProjecctC_CKIPTagger/content_CKIP.py
+++ ProjecctC_CKIPTagger/content_CKIP.py
@@ -1,8 +1,9 @@
 from ckiptagger import WS, POS, NER #導入CKIPTAGGER
 
-ws = WS("\home\dfr1238\CKIPtagger_data\data") #指定到訓練資料
-pos = POS("\home\dfr1238\CKIPtagger_data\data") #指定到訓練資料
-ner = NER("\home\dfr1238\CKIPtagger_data\data") #指定到訓練資料
+ckip_Data="/home/dfr1238/CKIPtagger_data/data" #訓練資料路徑
+ws = WS(ckip_Data) #指定到訓練資料
+pos = POS(ckip_Data) #指定到訓練資料
+ner = NER(ckip_Data) #指定到訓練資料
 
 sentence_list = ["傅達仁今將執行安樂死，卻突然爆出自己20年前遭緯來體育台封殺，他不懂自己哪裡得罪到電視台。",
 "美國參議院針對今天總統布什所提名的勞工部長趙小蘭展開認可聽 證會，預料她將會很順利通過參議院支持，成為該國有史以來第一位的華裔女性內閣成員。",
