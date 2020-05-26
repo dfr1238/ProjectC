4ccc895ccf9ed3efcd73e08e3d629bf8123564da
Douggy Sans <dfr1238@gmail.com>
2020-05-26 15:28:23 +0800

    公視爬蟲更新
    以css class name代替掉xpath的抓取，來避免路徑的變動導致抓取出錯

--- ProjectC_Spider/ProjectC_Spider/spiders/pts_news_crawl.py
+++ ProjectC_Spider/ProjectC_Spider/spiders/pts_news_crawl.py
@@ -3,15 +3,15 @@ from bs4 import BeautifulSoup
 class ptsNewsSpider(scrapy.Spider):
     name = "news.pts.org.tw" #爬蟲名稱
     allowed_domains = ['news.pts.org.tw/'] #允許網域
-    start_urls = ['https://news.pts.org.tw/article/473677'] #起始網址
+    start_urls = ['https://news.pts.org.tw/article/473677','https://news.pts.org.tw/article/480307'] #起始網址
 
     def parse(self, response):
         content = ''
         for news in response.css('body.newscont-page'):
-                title = news.xpath('/html/body/section/div/div/div[1]/h1/text()').get(), #獲取標題
-                time = news.xpath('/html/body/section/div/div/div[1]/div/div[1]/div[1]/h2/text()').get(), #獲取文章時間
-                author = news.xpath('/html/body/section/div/div/div[1]/div/div[1]/div[1]/div/text()').getall(), #獲取作者
-                content_html = news.xpath('/html/body/section/div/div/div[1]/div/div[2]/div[1]/div[4]').get(), #獲取內文HTML
+                title = news.css('.article-title::text').get(), #獲取標題
+                time = news.css('div.hidden-sm:nth-child(1) > h2:nth-child(1)::text').get(), #獲取文章時間
+                author = news.css('div.hidden-sm:nth-child(1) > div:nth-child(2)::text').getall(), #獲取作者
+                content_html = news.css('.article_content').get(), #獲取內文HTML
                 content= ''.join('%s' %id for id in content_html) #將內文HTML轉換成String
                 soup = BeautifulSoup(content,"lxml") #創立BS4處理
                 content=soup.get_text() #使用BS4將HTML轉至文字
