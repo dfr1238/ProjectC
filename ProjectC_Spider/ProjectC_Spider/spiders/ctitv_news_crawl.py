6c97abc5cf9ddf7c227ac8071125ce72d872dcfa
jojo <65437557+joen0925@users.noreply.github.com>
2020-06-06 20:07:00 +0800

    yes
    yes

--- ProjectC_Spider/ProjectC_Spider/spiders/ctitv_news_crawl.py
+++ ProjectC_Spider/ProjectC_Spider/spiders/ctitv_news_crawl.py
@@ -6,7 +6,9 @@ from bs4 import BeautifulSoup
 class CtitvSpider(scrapy.Spider):
     name = "gotv.ctitv.com.tw"#爬蟲名稱
     allowed_domains = ['gotv.ctitv.com.tw/']#允許網域
-    start_urls = ['https://gotv.ctitv.com.tw/2020/05/1285130.htm']#起始網址
+    def start_requests(self):#更換網址
+        for a in self.keywordsSearch():
+            yield scrapy.Request(a)
     def parse(self, response):
         for n in response.css('body'):
             title = n.css('h1.post-title.item.fn::text').get(default = '沒有抓到')#獲取標題
@@ -24,4 +26,17 @@ class CtitvSpider(scrapy.Spider):
                 'content' : content,
                 'source' : source
             }
-            
+    def SearchHref(self,keywords):#抓標題網址
+        google = 'https://www.google.com'
+        GoogleSearch = 'https://www.google.com/search?q=site:gotv.ctitv.com.tw%20'#ctitv網域
+        url = GoogleSearch + keywords
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text,'html.parser')
+        get_href = soup.select('div.kCrYT a')
+        for g in get_href:
+            yield google + g.get('href')
+
+    def keywordsSearch(self):#收尋關鍵字
+        keywords = str(input("輸入您要收尋的關鍵字:"))
+        keywords_url = self.SearchHref(keywords)#進入SearchHref()方法
+        return keywords_url   
