5158d50a7b07192804ad042b64673e47c6d3a90b
jojo <65437557+joen0925@users.noreply.github.com>
2020-06-23 13:45:18 +0800

    增加功能

--- ProjectC_Spider/ProjectC_Spider/spiders/ctitv_news_crawl.py
+++ ProjectC_Spider/ProjectC_Spider/spiders/ctitv_news_crawl.py
@@ -4,17 +4,23 @@ from bs4 import BeautifulSoup
 
 
 class CtitvSpider(scrapy.Spider):
+    keywords = ''
     name = "gotv.ctitv.com.tw"#爬蟲名稱
     allowed_domains = ['gotv.ctitv.com.tw/']#允許網域
-    def start_requests(self):#更換網址
-        for a in self.keywordsSearch():
-            yield scrapy.Request(a)
+
+    def start_requests(self):#爬蟲開始
+        CtitvSpider.SearchKeywords()
+        for page in range(3):#爬取3頁
+            for new in self.CrawlHref(page):#回傳網址
+                yield scrapy.Request(new, callback=self.parse)
+
     def parse(self, response):
         for n in response.css('body'):
             title = n.css('h1.post-title.item.fn::text').get(default = '沒有抓到')#獲取標題
             #處理GOTV特殊標題格式
             title = title.replace('\n','')#去掉title(str)多餘的\n
             title = title.replace('\t','')#去掉title(str)多餘的\t
+            title = title.replace('\u3000','')#去掉title(str)多餘的\u3000
             time = n.css('time.value-title::text').get(default = '沒有抓到')#獲取文章日期、時間
             author = n.css('span.reviewer a::text').get(default = '沒有抓到')#獲取作者
             content = n.css('div.post-content.description p::text').getall()#獲取文章內容
@@ -26,17 +32,15 @@ class CtitvSpider(scrapy.Spider):
                 'content' : content,
                 'source' : source
             }
-    def SearchHref(self,keywords):#抓標題網址
+    def CrawlHref(self,page):#抓每一頁的網址
         google = 'https://www.google.com'
-        GoogleSearch = 'https://www.google.com/search?q=site:gotv.ctitv.com.tw%20'#ctitv網域
-        url = GoogleSearch + keywords
-        r = requests.get(url)
+        GoogleSearch = 'https://www.google.com/search?q=site:gotv.ctitv.com.tw+' + self.keywords +'&start=' + str(page) + '0&sa=N'#ctitv網域
+        r = requests.get(GoogleSearch)
         soup = BeautifulSoup(r.text,'html.parser')
         get_href = soup.select('div.kCrYT a')
         for g in get_href:
             yield google + g.get('href')
 
-    def keywordsSearch(self):#收尋關鍵字
-        keywords = str(input("輸入您要收尋的關鍵字:"))
-        keywords_url = self.SearchHref(keywords)#進入SearchHref()方法
-        return keywords_url   
+    @classmethod
+    def SearchKeywords(cls):#關鍵字嵌入
+        cls.keywords = str(input("輸入您要收尋的關鍵字:"))
