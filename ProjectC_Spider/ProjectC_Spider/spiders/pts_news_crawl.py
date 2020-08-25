45d14158b542e9e05596127bf5d1511dfb5d198a
dfr1238 <dfr1238@gmail.com>
2020-08-26 02:55:45 +0800

    (公視新聞)針對新的版面調整
    調整爬蟲針對路徑來Match新的版面

--- ProjectC_Spider/ProjectC_Spider/spiders/pts_news_crawl.py
+++ ProjectC_Spider/ProjectC_Spider/spiders/pts_news_crawl.py
@@ -7,11 +7,11 @@ class ptsNewsSpider(scrapy.Spider):
 
     def parse(self, response):
         content = ''
-        for news in response.css('body.newscont-page'):
-                title = news.css('.article-title::text').get(), #獲取標題
-                time = news.css('div.hidden-sm:nth-child(1) > h2:nth-child(1)::text').get(), #獲取文章時間
-                author = news.css('div.hidden-sm:nth-child(1) > div:nth-child(2)::text').getall(), #獲取作者
-                content_html = news.css('.article_content').get(), #獲取內文HTML
+        for news in response.css('body'):
+                title = news.css('body > div.main-info.article-main-info > div > div > div > h1::text').get(), #獲取標題
+                time = news.css('body > div.main-info.article-main-info > div > div > div > div.text-muted.article-info > time::text').get(), #獲取文章時間
+                author = news.css('body > div.main-info.article-main-info > div > div > div > div.text-muted.article-info > span.article-reporter.mr-2::text').getall(), #獲取作者
+                content_html = news.css('body > div.container > div > div.col-lg-6 > article').get(), #獲取內文HTML
                 content= ''.join('%s' %id for id in content_html) #將內文HTML轉換成String
                 soup = BeautifulSoup(content,"lxml") #創立BS4處理
                 content=soup.get_text() #使用BS4將HTML轉至文字
